633906c03e7d5d88668a3b6bd5dc267293714984
will brigida <will_brigida@hotmail.com>
2019-05-01 11:46:47 -0300

    Primeiro Commit

---
+++ EstudoPrism/EstudoPrism/App.xaml.cs
@@ -0,0 +1,51 @@
+﻿using EstudoPrism.ViewModels;
+using Prism;
+using Prism.DryIoc;
+using Prism.Ioc;
+using Prism.Navigation;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EstudoPrism
+{
+    public partial class App : PrismApplication
+    {
+        public App()
+            : this(null)
+        {
+
+        }
+
+        public App(IPlatformInitializer initializer)
+            : this(initializer, true)
+        {
+
+        }
+
+        public App(IPlatformInitializer initializer, bool setFormsDependencyResolver)
+            : base(initializer, setFormsDependencyResolver)
+        {
+
+        }
+
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+            await NavigationService.NavigateAsync("/NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+
+            containerRegistry.RegisterForNavigation<MainPage, MainViewModel>();
+            //containerRegistry.RegisterForNavigation<DetalhesPage, DetalhesViewModel>();
+            //containerRegistry.RegisterForNavigation<CardsPage, CardsViewModel>();
+            //containerRegistry.RegisterForNavigation<MenuPage, MenuViewModel>();
+
+            //containerRegistry.RegisterSingleton<IMarvelApiService, MarvelApiService>();
+        }
+    }
+}
---
+++ EstudoPrism/EstudoPrism/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Prism;
+using Prism.Navigation;
+using Prism.Services;
+
+namespace EstudoPrism.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainViewModel(INavigationService navigationService,
+                IPageDialogService pageDialogService) : base(navigationService, pageDialogService)
+        {
+        }
+    }
+}
